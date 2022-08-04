9bb4c8873cbb9db299d28af180389d102d65caa1
Timo De Waele <timo.dewaele@ugent.be>
2022-08-04 22:39:59 +0200

    Added some structs and functions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,25 @@
+#[derive(Debug)]
+struct Pixel{
+    r: u8,
+    g: u8,
+    b: u8
+}
+
+impl Pixel {
+    fn add_intensity(& mut self, i: i8) {
+        self.r = std::cmp::max(std::cmp::min(0, self.r as i8 + i) as u8, 255);
+    }
+}
+
+
+#[derive(Debug)]
+struct Canvas{
+    width: u32,
+    height: u32,
+}
+
+
 fn main() {
-    let name = String::from("Timo");
-    println!("Hello, {}!", name);
+    let p_1 = Pixel { r: 123, g: 234, b: 56 };
+    println!("Pixel: {:?}!", p_1);
 }
